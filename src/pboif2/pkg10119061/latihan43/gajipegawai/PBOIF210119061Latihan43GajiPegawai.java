f57b0baf0241ee5f6436537b1b9d2cbea8516c97
ejaakml <eximindotong@yahoo.com>
2020-11-14 18:43:22 +0700

    43

---
+++ src/pboif2/pkg10119061/latihan43/gajipegawai/PBOIF210119061Latihan43GajiPegawai.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119061.latihan43.gajipegawai;
+
+/**
+ *
+ * @author Rheiza
+ * NAMA     : Rheiza Akmal R
+ * KELAS    : IF2
+ * NIM      : 10119061
+
+ */
+public class PBOIF210119061Latihan43GajiPegawai {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
